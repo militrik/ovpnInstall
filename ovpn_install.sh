dc6c93d1964ad2bf1fab15f32340e7abfff9334d
militrik <militrik@users.noreply.github.com>
2022-04-28 13:39:35 +0300

    Update ovpn_install.sh

--- ovpn_install.sh
+++ ovpn_install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-$ apt update
-$ apt install -y bridge-utils dmidecode iptables iproute2 libc6 libffi7 libgcc-s1 liblz4-1 liblzo2-2 libmariadb3 libpcap0.8 libssl1.1 libstdc++6 zlib1g libsasl2-2 libsqlite3-0 net-tools python3-pkg-resources python3-migrate python3-sqlalchemy python3-mysqldb python3-ldap3 sqlite3
-$ dpkg -i openvpn-as-bundled-clients-24.deb openvpn-as_2.10.2-3383e1e5-Debian11_amd64.deb
+apt update
+apt install -y bridge-utils dmidecode iptables iproute2 libc6 libffi7 libgcc-s1 liblz4-1 liblzo2-2 libmariadb3 libpcap0.8 libssl1.1 libstdc++6 zlib1g libsasl2-2 libsqlite3-0 net-tools python3-pkg-resources python3-migrate python3-sqlalchemy python3-mysqldb python3-ldap3 sqlite3
+dpkg -i openvpn-as-bundled-clients-24.deb openvpn-as_2.10.2-3383e1e5-Debian11_amd64.deb
