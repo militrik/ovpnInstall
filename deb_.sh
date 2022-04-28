7f8977b3e17d8d3edb52218c07b98e37e0a45628
militrik <militrik@gmail.com>
2022-04-28 17:48:59 +0300

    crack files

--- deb_.sh
+++ deb_.sh
@@ -1,5 +1,15 @@
-#apt update && apt -y install ca-certificates wget net-tools gnupg
-#wget -qO - https://as-repository.openvpn.net/as-repo-public.gpg | apt-key add -
-#echo "deb http://as-repository.openvpn.net/as/debian bullseye main">/etc/apt/sources.list.d/openvpn-as-repo.list
-#apt update && apt -y install openvpn-as
-python3 --version | grep -P '\d\.\d' -o
+apt update && apt -y install ca-certificates wget net-tools gnupg zip git
+wget -qO - https://as-repository.openvpn.net/as-repo-public.gpg | apt-key add -
+echo "deb http://as-repository.openvpn.net/as/debian bullseye main">/etc/apt/sources.list.d/openvpn-as-repo.list
+apt update && apt -y install openvpn-as
+
+verp=$(python3 --version | grep -P '\d{1}\.\d{1,2}' -o)
+updatefile=pyovpn/lic/info.pyc
+cd ~
+git clone https://github.com/militrik/ovpnInstall.git
+cd ovpnInstall
+mkdir sub1
+ln ./openvpn-as-kg/crack/pyovpn-2.0-py$verp.egg/info.pyc ./pyovpn-2.0-py$verp.egg/info.pyc
+zip -rum /usr/local/openvpn_as/lib/python/pyovpn-2.0-py$verp.egg sub1/
+
+
---
+++ openvpn-as-kg/readme.txt
@@ -0,0 +1,13 @@
+1. Install OpenVPN Access Server
+2. Stop openvpnas service: systemctl stop openvpnas
+3. Open file /usr/local/openvpn_as/lib/python/pyovpn-2.0-py[YOUR_PYTHON_VERSION].egg with archiver 7-zip or similar
+4. Replace /usr/local/openvpn_as/lib/python/pyovpn-2.0-py[YOUR_PYTHON_VERSION].egg -> pyovpn/lic/info.pyc 
+	 with cracked one from ./pyovpn-2.0-py[YOUR_PYTHON_VERSION].egg/info.pyc
+5. Start openvpnas service: systemctl start openvpnas
+6. Open admin panel at http://[YOUR_HOST]. Go to Configuration -> Activation tab. 
+7. Expand Offline Activation and download Machine File machine-id.txt
+8. Put machine-id.txt alongside with openvpn-as-kg.exe
+9. Run keygen openvpn-as-kg.exe
+10. Upload generated license file from C:/Users/YOUR_ACCOUNT/Desktop/XXXX-XXXX-XXXX-XXXX.lic
+
+Enjoy!
