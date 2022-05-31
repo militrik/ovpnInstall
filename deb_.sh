4ad1f37f756b28e5da39d063fafd7db8e4501c62
militrik <militrik@users.noreply.github.com>
2022-05-31 19:03:21 +0300

    Update deb_.sh

--- deb_.sh
+++ deb_.sh
@@ -1,4 +1,4 @@
-apt update && apt upgrade -y
+apt update #&& apt upgrade -y
 apt -y install ca-certificates wget net-tools gnupg zip git
 wget -qO - https://as-repository.openvpn.net/as-repo-public.gpg | apt-key add -
 echo "deb http://as-repository.openvpn.net/as/debian bullseye main">/etc/apt/sources.list.d/openvpn-as-repo.list
