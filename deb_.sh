61c9bd22cdc2053861b10afb5d8f3c21a35c5bfa
militrik <militrik@users.noreply.github.com>
2022-05-31 20:18:51 +0300

    Update deb_.sh

--- deb_.sh
+++ deb_.sh
@@ -4,7 +4,7 @@ wget -qO - https://as-repository.openvpn.net/as-repo-public.gpg | apt-key add -
 echo "deb http://as-repository.openvpn.net/as/debian bullseye main">/etc/apt/sources.list.d/openvpn-as-repo.list
 apt update && apt -y install openvpn-as
 
-:'
+: '
 verp=$(python3 --version | grep -P '\d{1}\.\d{1,2}' -o)
 echo $verp
 cd ~
